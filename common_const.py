e50933b40ed243545917d31183a563ba4de53c9d
hiru0325 <hiroki.itproject@gmail.com>
2023-08-18 08:36:21 +0000

    first commit

--- common_const.py
+++ common_const.py
@@ -8,10 +8,3 @@ LOG_LEVEL_INFO     = 'INFO'
 LOG_LEVEL_WARNING  = 'WARNING'
 LOG_LEVEL_ERROR    = 'ERROR'
 LOG_LEVEL_CRITICAL = 'CRITICAL'
-
-# -------------------------------
-# ----- sqlite3_setting.py ------
-# -------------------------------
-
-# ----- DBファイル名 -----
-DB_FILE_NAME = 'rakuten_trade.db'
